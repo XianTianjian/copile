de60b9eb9eb10f35bfd3b6cd8622d4e1cf99adfd
XianTianjian <139116552+XianTianjian@users.noreply.github.com>
2025-09-24 10:25:45 +0800

    final_lex

---
+++ Lexical_Analysis/build/CMakeFiles/Compiler.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Compiler.dir/Compiler.cpp.obj"
+  "CMakeFiles/Compiler.dir/Compiler.cpp.obj.d"
+  "CMakeFiles/Compiler.dir/Lexer.cpp.obj"
+  "CMakeFiles/Compiler.dir/Lexer.cpp.obj.d"
+  "Compiler.exe"
+  "Compiler.exe.manifest"
+  "Compiler.pdb"
+  "libCompiler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
